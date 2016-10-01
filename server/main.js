8a17e502adc68cd736112581067c72e85c0d0040
Levente Bagi <bagilevi@gmail.com>
2016-10-01 16:33:18 +0100

    get dependencies from backend

--- server/main.js
+++ server/main.js
@@ -1,8 +1,14 @@
+var jsModuleDependecies = require('js-module-dependencies');
+var layerizer = require('layerizer');
+
 module.exports = function(app) {
   var bodyParser = require('body-parser');
   app.use(bodyParser.json());
 
-  app.get('/', function (req, res) {
-    res.send('dependency-visualizer');
+  app.get('/projects/1.json', function (req, res) {
+    jsModuleDependecies.getModuleDependenciesInProject('/Users/lev/tmp/react-dnd/src', function(dependencies) {
+      res.send(JSON.stringify(dependencies, null, 2));
+    });
+
   });
 }
