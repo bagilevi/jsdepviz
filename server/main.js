803386adaaf9d238969f363a4c2ffca7506fa353
Levente Bagi <bagilevi@gmail.com>
2016-10-01 16:38:55 +0100

    attempt to get layers

--- server/main.js
+++ server/main.js
@@ -1,5 +1,6 @@
 var jsModuleDependecies = require('js-module-dependencies');
 var layerizer = require('layerizer');
+var graphlib = require('graphlib');
 
 module.exports = function(app) {
   var bodyParser = require('body-parser');
@@ -7,8 +8,21 @@ module.exports = function(app) {
 
   app.get('/projects/1.json', function (req, res) {
     jsModuleDependecies.getModuleDependenciesInProject('/Users/lev/tmp/react-dnd/src', function(dependencies) {
-      res.send(JSON.stringify(dependencies, null, 2));
+      var graph = convertDependenciesToGraph(dependencies);
+      var layers = layerizer.layerize(graph);
+      res.send(JSON.stringify(layers, null, 2));
     });
 
   });
+}
+
+function convertDependenciesToGraph(dependencies) {
+  var g = new graphlib.Graph();
+  for (var src in dependencies) {
+    var dests = dependencies[src];
+    for (var i = 0; i < dests.length; i++) {
+      g.setEdge(src, dests[i]);
+    }
+  }
+  return g;
 }
