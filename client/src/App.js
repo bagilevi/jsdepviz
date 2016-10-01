64089641c0dc2b2157c574a5b94c0f73fa88f5bd
Levente Bagi <bagilevi@gmail.com>
2016-10-01 18:08:53 +0100

    display layers

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,7 @@ import $ from "jquery";
 
 // import DevTools from 'mobx-react-devtools';
 
-let data = observable({message: "Hello World!", project: null});
+let data = observable({project: null});
 
 $.ajax({
   url: "http://0.0.0.0:3001/projects/1.json",
@@ -19,9 +19,43 @@ $.ajax({
   render() {
     return (
       <div id="main">
-        {data.message}
-        <pre>{JSON.stringify(data.project, null, 2)}</pre>
+        {data.project !== null ? <Layers layers={data.project.layers.reverse()}/> : null}
+        {/*<pre>{JSON.stringify(data.project, null, 2)}</pre>*/}
       </div>
     );
   }
 }
+
+class Layers extends Component {
+  render() {
+    return (
+      <div id="layers">
+        {
+          this.props.layers.map((layer) => (
+            <div className="layer">
+              {
+                layer.map((cluster) => (
+                  <div className="cluster">
+                    {
+                      cluster.map((node) => (
+                        <div className="node">
+                          {node}
+                        </div>
+                      ))
+                    }
+                  </div>
+                ))
+              }
+            </div>
+          ))
+        }
+      </div>
+    );
+  }
+}
+
+// Components:
+// * Layer
+// * Cluster
+// * Module (js file) - can hover
+// * Edge (js file to js file) - can be highlighted
