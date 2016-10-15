00d690b48e788c4dd8e215b16f7b8289b2b8c89e
Levente Bagi <bagilevi@gmail.com>
2016-10-15 10:57:45 +0100

    cache with redis

--- server/main.js
+++ server/main.js
@@ -5,8 +5,46 @@ var gitRepoRetriever = require('./git-repo-retriever');
 
 var repoDir = '../data/repos';
 
+var redis = require("redis"),
+    redisClient = redis.createClient();
+
 function getDistances(graph) {
   return graphlib.alg.dijkstraAll(graph);
+}
+
+function getProjectJSON(user, repo, ref, callback) {
+  gitRepoRetriever.getRepoFromGithub(user, repo, null, repoDir, (err, path) => {
+    if (err) return callback(err);
+    jsModuleDependecies.getModuleDependenciesInProject(path, function(dependencies) {
+      var graph = convertDependenciesToGraph(dependencies);
+      var layers = layerizer.layerize(graph);
+      var project = {
+        dependencies: dependencies,
+        layers: layers,
+        distances: getDistances(graph)
+      }
+      json = JSON.stringify(project, null, 2);
+      callback(null, json);
+    });
+  })
+}
+
+function withCache(key, func, callback) {
+  redisClient.get(key, function(err, reply) {
+    if (err) return callback(`Redis: ${err}`);
+    if (reply) return callback(null, reply);
+    func((err, value) => {
+      if (err) return callback(err);
+      redisClient.set(key, value, function(err, reply) {
+        if (err) return callback(`Redis: ${err}`);
+        callback(null, value);
+      })
+    });
+  });
+
+}
+function getProjectJSONWithCache(user, repo, ref, callback) {
+  withCache(`jsdepviz:projectjson:${user}/${repo}/${ref || '@'}`, getProjectJSON.bind(this, user, repo, ref), callback);
 }
 
 module.exports = function(app) {
@@ -18,19 +56,12 @@ module.exports = function(app) {
     try {
       var user = req.params.user;
       var repo = req.params.repo;
-      gitRepoRetriever.getRepoFromGithub(user, repo, null, repoDir, (err, path) => {
+
+      getProjectJSONWithCache(user, repo, null, (err, json) => {
         if (err) return respondWithError(err);
-        jsModuleDependecies.getModuleDependenciesInProject(path, function(dependencies) {
-          var graph = convertDependenciesToGraph(dependencies);
-          var layers = layerizer.layerize(graph);
-          var project = {
-            dependencies: dependencies,
-            layers: layers,
-            distances: getDistances(graph)
-          }
-          res.send(JSON.stringify(project, null, 2));
-        });
-      })
+        res.send(json);
+      });
+
     } catch (e) {
       respondWithError(e);
     }
